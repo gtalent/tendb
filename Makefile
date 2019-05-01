067e06a0512e17d05cd5babf5c9f2c58509b38bd
Gary Talent <gtalent2@gmail.com>
2019-05-01 07:20:07 -0500

    Add assets file

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ start-postgres:
 	docker run --restart=always --name tendb-postgres -e POSTGRES_DB=tendb -e POSTGRES_PASSWORD=postgres -v /var/lib/10db/pgdata/:/var/lib/postgresql/data -p 5432:5432 -d postgres:alpine
 pg-cli:
 	docker exec -ti tendb-postgres psql -h localhost -U postgres -d tendb
+generate:
+	go generate github.com/gtalent/tendb/db
--- db/01_migration.go
+++ db/01_migration.go
@@ -1,16 +1,8 @@
-/*
-   Copyright 2017 - 2019 gtalent2@gmail.com
-
-   This Source Code Form is subject to the terms of the Mozilla Public
-   License, v. 2.0. If a copy of the MPL was not distributed with this
-   file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
 
 package db
 
 import (
 	"github.com/go-pg/migrations"
-	"log"
 )
 
 func init() {
@@ -19,7 +11,6 @@ func init() {
 		path := dir + "/up.sql"
 		sql, err := loadMigrationFile(path)
 		if err != nil {
-			log.Println(err)
 			return err
 		}
 		_, err = db.Exec(sql)
@@ -28,8 +19,6 @@ func init() {
 		path := dir + "/down.sql"
 		sql, err := loadMigrationFile(path)
 		if err != nil {
-			log.Println(err)
-			return err
 			return err
 		}
 		_, err = db.Exec(sql)
